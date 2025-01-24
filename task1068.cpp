25d83d68d4f4ede852da93416abea631d0e8468c
youna <youannabanyamin@gmail.com>
2025-01-24 13:52:45 +0100

    DONE

--- task1068.cpp
+++ task1068.cpp
@@ -1,13 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n; cin >> n;
-    while(n != 1){
-	cout << n << ' ' << endl;
-        if(n%2 == 0) n = n/2; 
-        else n = n*3+1; 
-    }
-	cout << 1;
-    return 0;
+int main() {
+    for (int n; cin >> n; cout << n << " \n"[n == 1], n = n % 2 ? n * 3 + 1 : n / 2);
 }
