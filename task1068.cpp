b16e6ac0e1a46d335a19ed9b377eba8bf7ce4beb
youna <youannabanyamin@gmail.com>
2025-01-24 11:52:54 +0100

    Update task1068.cpp

--- task1068.cpp
+++ task1068.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n; cin >> n;
     while(n != 1){
-		cout << n << ' ' << endl;
+	cout << n << ' ' << endl;
         if(n%2 == 0) n = n/2; 
         else n = n*3+1; 
     }
