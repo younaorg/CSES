a6d97b733004a2545ba3971963b1974534f1265e
youna <youannabanyamin@gmail.com>
2025-01-24 13:50:06 +0100

    DONE

--- task1070.cpp
+++ task1070.cpp
@@ -3,14 +3,11 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-
     if (n < 4 && n > 1) {
         cout << "NO SOLUTION" << endl;
         return 0;
     }
-
     for (int i = 2; i <= n; i += 2) cout << i << ' ';
     for (int i = 1; i <= n; i += 2) cout << i << ' ';
-
     return 0;
 }
