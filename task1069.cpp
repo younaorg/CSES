6783f0d23232ec92d68e5a65ebe505dfa2cede36
youna <youannabanyamin@gmail.com>
2025-01-24 13:46:12 +0100

    DONE

---
+++ task1069.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    int res = 1, c = 1;
+    for (int i = 1; i < s.size(); i++) 
+        res = max(res, c = (s[i] == s[i - 1] ? c + 1 : 1));
+    cout << res << endl;
+}
