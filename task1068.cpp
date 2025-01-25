235d1401922c95038a673c40a3a1e31356dfac05
youna <youannabanyamin@gmail.com>
2025-01-25 14:37:30 +0100

    FINAL

--- task1068.cpp
+++ task1068.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 
 int main() {
-    for (int n; cin >> n; cout << n << " \n"[n == 1], n = n % 2 ? n * 3 + 1 : n / 2);
+    int n; cin >> n; 
+    for(; cout << n << "\n", n!=1; n = n%2 ? 3*n+1 : n/2);
 }
