1fc225e58dba3ea2efb565213bf63049bff553e2
youna <youannabanyamin@gmail.com>
2025-01-25 14:39:42 +0100

    FINAL

--- task1069.cpp
+++ task1069.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     string s; cin >> s;
-    int res = 1, c = 1;
-    for (int i = 1; i < s.size(); i++) 
-        res = max(res, c = (s[i] == s[i - 1] ? c + 1 : 1));
+    int res, c = 1;
+    for (int i=1; i<s.size(); i++) 
+        res = max(res, c = (s[i] == s[i-1] ? c+1 : 1));
     cout << res << endl;
 }
